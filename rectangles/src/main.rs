e3d63ebdba0887f5ea43ea1b5043bf5082b477b5
Tackmas <tahmassarwari@gmail.com>
2025-11-13 22:15:44 +0100

    Completed 'rectangles' project.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+struct Rectangle {
+    width: usize,
+    height: usize,
 }
+
+impl Rectangle {
+    fn area(&self) -> usize {
+        return self.width * self.height
+    }
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 10,
+        height: 12
+    };
+
+    println!("{}", rect.area());
+}   
